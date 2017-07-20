1bf56b44bf493fab6cee52e56c53dbb520b61ed5
TarasMatsyshyn <taras.matsyshyn@gmail.com>
2017-07-20 14:34:12 +0300

    fixed sume bugs'

--- src/main/java/abstractMethod/BattleDroid.java
+++ src/main/java/abstractMethod/BattleDroid.java
@@ -5,6 +5,6 @@ package abstractMethod;
  */
 public class BattleDroid implements Droid {
     public void sayHello() {
-        System.out.println("Hello I`m battle droid");
+        System.out.println("Hello, I`m battle droid");
     }
 }
--- src/main/java/abstractMethod/TransportDroid.java
+++ src/main/java/abstractMethod/TransportDroid.java
@@ -2,6 +2,6 @@ package abstractMethod;
 
 public class TransportDroid implements Droid {
     public void sayHello() {
-        System.out.println("Hello I`m transport droid");
+        System.out.println("Hello, I`m transport droid");
     }
 }
